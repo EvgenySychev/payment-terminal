9ef632eea96b35cb24dc1f770c6e2814189c808f
EvgenySychev <samara808@yandex.ru>
2022-11-09 13:51:11 +0400

    added CurrencyInput in paymentPage.tsx

--- pages/paymentPage.tsx
+++ pages/paymentPage.tsx
@@ -1,24 +1,36 @@
 import OperatorItem from '../components/OperatorItem'
 import PhoneInput from 'react-phone-input-2'
+import CurrencyInput from 'react-currency-input-field'
 import 'react-phone-input-2/lib/style.css'
 
 type PaymentPagePropsType = {
-    key:number
-    title:string
+    key: number
+    title: string
 }
 
-const PaymentPage = ({ key,title }: PaymentPagePropsType) => {
+const PaymentPage = ({key, title}: PaymentPagePropsType) => {
 
     return <>
-    <OperatorItem key={key} title={'title'}/>
+        <OperatorItem key={key} title={'title'}/>
         <div>
-            <PhoneInput
-                country={'ru'}
-                preferredCountries={['ru']}
-                inputProps={{}}
-            />
-            <input type="text" name="" id=""/>
-            <button>Оплатить</button>
+            <div>
+                <PhoneInput
+                    country={'ru'}
+                    preferredCountries={['ru']}
+                    inputProps={{}}
+                />
+            </div>
+            <div>
+                <CurrencyInput
+                    placeholder="Введите сумму"
+                    defaultValue={1000}
+                    decimalsLimit={2}
+                    prefix={'₽ '}
+                />
+            </div>
+            <div>
+                <button>Оплатить</button>
+            </div>
         </div>
     </>
 }
