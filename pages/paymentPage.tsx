e0636a0d9eb1d2fb412f7b8438021d14ae2b86da
EvgenySychev <samara808@yandex.ru>
2022-11-09 14:34:27 +0400

    added validation without a mask

--- pages/paymentPage.tsx
+++ pages/paymentPage.tsx
@@ -1,36 +1,70 @@
 import OperatorItem from '../components/OperatorItem'
-import PhoneInput from 'react-phone-input-2'
-import CurrencyInput from 'react-currency-input-field'
-import 'react-phone-input-2/lib/style.css'
+import { useFormik } from "formik";
 
 type PaymentPagePropsType = {
     key: number
     title: string
 }
 
+type FormikErrorType = {
+    phoneNumber?: string
+    amountMoney?: string
+}
+
 const PaymentPage = ({key, title}: PaymentPagePropsType) => {
+
+    const formik = useFormik({
+        initialValues: {
+            phoneNumber: '',
+            amountMoney: '1000',
+        },
+        validate: (values) => {
+            const errors: FormikErrorType = {};
+            if (!values.phoneNumber) {
+                errors.phoneNumber = 'Обязательно';
+            } else if (values.phoneNumber.length < 11) {
+                errors.phoneNumber = 'Неверный формат номера';
+            }
+
+            if (!values.amountMoney) {
+                errors.amountMoney = 'Обязательно';
+            } else if (Number(values.amountMoney) < 1) {
+                errors.amountMoney = 'Сумма может быть больше 0 ₽';
+            } else if (Number(values.amountMoney) > 1000) {
+                errors.amountMoney = 'Сумма не может быть больше 1000 ₽';
+            }
+            return errors;
+        },
+        onSubmit: values => {
+            formik.resetForm()
+            alert('оплачно')
+        },
+    })
 
     return <>
         <OperatorItem key={key} title={'title'}/>
         <div>
-            <div>
-                <PhoneInput
-                    country={'ru'}
-                    preferredCountries={['ru']}
-                    inputProps={{}}
-                />
-            </div>
-            <div>
-                <CurrencyInput
-                    placeholder="Введите сумму"
-                    defaultValue={1000}
-                    decimalsLimit={2}
-                    prefix={'₽ '}
-                />
-            </div>
-            <div>
-                <button>Оплатить</button>
-            </div>
+            <form onSubmit={formik.handleSubmit}>
+                <div>
+                    <input
+                        placeholder={'+7 (...) ... - .. - .. '}
+                        {...formik.getFieldProps("phoneNumber")}
+                    />
+                </div>
+                {formik.touched.phoneNumber && formik.errors.phoneNumber ?
+                    <div style={{color: 'red'}}>{formik.errors.phoneNumber}</div> : null}
+                <div>
+                    <input
+                        placeholder="Введите сумму"
+                        {...formik.getFieldProps("amountMoney")}
+                    />
+                </div>
+                {formik.touched.amountMoney && formik.errors.amountMoney ?
+                    <div style={{color: 'red'}}>{formik.errors.amountMoney}</div> : null}
+                <div>
+                    <button>Оплатить</button>
+                </div>
+            </form>
         </div>
     </>
 }
